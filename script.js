abe5187c7c85860ffc22ca73e6f3c277cd06f97d
su3-kai <susan10762@icloud.com>
2026-06-16 14:45:12 +0900

    記号を含むパスワード生成機能を実装

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const passwordInput = document.getElementById("password"); // パスワード表
 const lengthInput = document.getElementById("length");  // 文字数指定欄
 const uppercaseCheckbox = document.getElementById("uppercase"); // 大文字チェックボックス
 const lowercaseCheckbox = document.getElementById("lowercase"); // 小文字チェックボックス
-const numbersCheckbox = document.getElementById("numbers");
+const numbersCheckbox = document.getElementById("numbers"); // 数字チェックボックス
+const symbolsCheckbox = document .getElementById("symbols"); // 記号チェックボックス
 
 generateBtn.addEventListener("click", () => {
     const length = Number(lengthInput.value);
@@ -18,6 +19,9 @@ generateBtn.addEventListener("click", () => {
     }
     if (numbersCheckbox.checked) {
         characters += "0123456789";
+    }
+    if (symbolsCheckbox.checked) {
+        characters += "._";
     }
 
     let password = "";
