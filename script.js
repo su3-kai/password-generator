00cf03dc1452f7fcabdec754835883b6a0c3257e
su3-kai <susan10762@icloud.com>
2026-06-16 14:29:50 +0900

    英大文字・英小文字によるパスワード生成機能を実装

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const generateBtn = document.getElementById("generate-btn");
 const passwordInput = document.getElementById("password");
+const lengthInput = document.getElementById("length");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
 
 generateBtn.addEventListener("click", () => {
-    const characters = "abcdefghijklmnopqrstuvwxyz";
+    const length = Number(lengthInput.value);
+
+    let characters = "";
+
+    if (uppercaseCheckbox.checked) {
+        characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (lowercaseCheckbox.checked) {
+        characters += "abcdefghijklmnopqrstuvwxyz";
+    }
+
     let password = "";
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         password += characters[randomIndex];
     }
